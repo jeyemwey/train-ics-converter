26ccbf05ffe76180556e83b70e9e4776a81861a7
Jannik <jannik@outlook.com>
2021-10-17 00:20:30 +0200

    :rocket: add health route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { decode, encode } from './binary-utils';
 import { client, Journey, Leg } from './hafas-client';
-import { legToEvent, toCalendar } from './ical';
+import { toCalendar } from './ical';
 
 const app = express();
 const port = 3000;
@@ -11,10 +11,10 @@ app.get('/', (req, res) => {
 });
 
 const lineDesc = (l: Leg) => {
-    if (typeof l.line !== "undefined" ){
+    if (typeof l.line !== "undefined") {
         return l.line?.name;
     }
-    
+
     if (l.walking!) {
         return "zu Fuß gehen"
     }
@@ -34,19 +34,19 @@ app.get('/journeys', async (req, res) => {
     const to = await client.locations(req.query.to as string, { results: 1 })
     const departure = new Date(req.query.departure as string);
 
-    if (departure.getTime() === NaN ) {
-        return res.status(400).send({error: "Bad Request: Bad departure time"})
+    if (departure.getTime() === NaN) {
+        return res.status(400).send({ error: "Bad Request: Bad departure time" })
     }
-    
+
     const journeys = (await client.journeys(from[0], to[0], {
         results: 10,
         departure: req.query.departure as string
     }))
-    .journeys.map(j => ({
-        journeyText: journeyText(j),
-        refreshToken: encode(j.refreshToken)
-    }))
-    
+        .journeys.map(j => ({
+            journeyText: journeyText(j),
+            refreshToken: encode(j.refreshToken)
+        }))
+
     return res.send({ journeys })
 })
 
@@ -56,9 +56,9 @@ app.get('/journeys', async (req, res) => {
  */
 app.get("/cal", async (req, res) => {
     const token_encoded = req.query.refreshToken;
-    if(!token_encoded)
-        return res.status(400).send({error: "Bad Request: refreshToken is not valid"})
-    
+    if (!token_encoded)
+        return res.status(400).send({ error: "Bad Request: refreshToken is not valid" })
+
     const token = decode(token_encoded as string);
 
     const journey = await client.refreshJourney(token)
@@ -68,6 +68,10 @@ app.get("/cal", async (req, res) => {
 });
 
 
+app.get('/_health', (req, res) => {
+    res.status(200).send('ok')
+})
+
 app.listen(port, () => {
-    return console.log(`server is listening on ${port}`);
+    return console.log(`server is listening on ${port}, running in ${process.env.NODE_ENV} mode`);
 });
